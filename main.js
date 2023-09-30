7675e4c75a06dc01709a547929457ce6251d440b
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-30 16:44:40 +0100

    Completed Assignment

--- main.js
+++ main.js
@@ -4,7 +4,32 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likes = document.querySelectorAll(".like-glyph");
 
+likes.forEach(like => {
+  like.addEventListener("click", () => {
+    mimicServerCall()
+      .then(res => {
+        const heartState = like.textContent;
+        if (heartState === EMPTY_HEART) {
+          like.textContent = FULL_HEART;
+          like.classList.add("activated-heart");
+        } else if (heartState === FULL_HEART) {
+          like.textContent = EMPTY_HEART;
+          like.classList.remove("activated-heart");
+        }
+      })
+      .catch(error => {
+        const modal = document.querySelector("#modal");
+        modal.classList.remove("hidden");
+        const p = document.querySelector("#modal-message");
+        p.textContent = error.message;
+        setTimeout(() => {
+          modal.classList.add("hidden");
+        }, 3000);
+      })
+  });
+})
 
 
 //------------------------------------------------------------------------------
